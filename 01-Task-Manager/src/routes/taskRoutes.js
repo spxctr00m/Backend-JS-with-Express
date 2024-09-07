92984804ed3a9b3eff34c48de7152500e983e9f4
Bramstone <en4abraham@gmail.com>
2024-09-07 09:20:48 +0100

    refactored routes to use controllers.js

--- 01-Task-Manager/src/routes/taskRoutes.js
+++ 01-Task-Manager/src/routes/taskRoutes.js
@@ -1,18 +1,13 @@
 const router = require("express").Router();
-const { getAllTasks, createTask } = require("../controllers/tasks");
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  deleteTask,
+  updateTask,
+} = require("../controllers/tasks");
 
 router.route("/").get(getAllTasks).post(createTask);
-
-router.get("/:taskid", (req, res) => {
-  res.json({ msg: "get specific task" });
-});
-
-router.patch("/:taskid", (req, res) => {
-  res.json({ msg: "edit task" });
-});
-
-router.delete("/:taskid", (req, res) => {
-  res.json({ msg: "delete task" });
-});
+router.route("/:taskId").get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
